9fca08c2f3260175fd0fa35ebbf42c8b31d86e41
PascalUlor <32112393+PascalUlor@users.noreply.github.com>
2017-09-24 21:13:37 +0100

    Created an input form to populate recipe table

--- templates/js/index.js
+++ templates/js/index.js
@@ -5,3 +5,17 @@ function main(){
   /*$(this).text('Projects Viewed');*/   });
 }
 $(document).ready(main);
+
+function addRow() {
+    var table = document.getElementById('results');
+    var row = document.createElement('tr');
+    var td1 = document.createElement('td');
+    var td2 = document.createElement('td');
+    var td3 = document.createElement('td');
+    td1.innerHTML = document.getElementById('recipe').value;
+    td2.innerHTML = document.getElementById('details').value;
+    td3.innerHTML = document.getElementById('review').value;
+    row.appendChild(td1);
+    row.appendChild(td2);
+    row.appendChild(td3);
+    table.children[0].insertBefore(row, table.children[0].childNodes[1]);}
