c6439b0d3f4231187ea230a48293fe9fd0ec9072
PascalUlor <32112393+PascalUlor@users.noreply.github.com>
2017-09-29 12:24:33 +0100

    create recipe model

---
+++ server/model/recipes.js
@@ -0,0 +1,38 @@
+const recipes = [
+        {
+            id:1,
+            title:'salad',
+            ingridient: ['cabbage', 'cucumber', 'carrot', 'green peas'],
+            description: 'vegetables',
+            upVote: 30,
+            downVote: 10,
+            favorites: 5,
+            views: 50,
+            review:[]
+        },
+
+        {
+            id:1,
+            title:'Jollof Rice',
+            ingridient: ['rice', 'green peas', 'sweet corn', 'chicken'],
+            description:'Carbohydrate',
+            upVote: 55,
+            downVote: 27,
+            favorites: 100,
+            views: 200,
+            review:[]
+        },
+        {
+            id:1,
+            title:'Afang Soup',
+            ingridient: ['goat meat', 'vegetable leave', 'palm oil', 'cray fish'],
+            description:'Vegetable, fat and oil',
+            upVote: 79,
+            downVote: 8,
+            favorites: 100,
+            views: 300,
+            review:[]
+        }
+
+        
+]
